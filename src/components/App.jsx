b1f5182e09776d6b0f2dc7f54294ea0732ad417c
Marko Divjak <marko.divjak@protonmail.com>
2017-10-06 18:54:03 +0200

    Boilerplate project with electron & react

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <p>Test if works!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
