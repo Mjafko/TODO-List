c6cec2a9d219271add8cbdf0833199fd45af2729
Marko Divjak <marko.divjak@protonmail.com>
2017-10-06 19:03:20 +0200

    Remove node_modules for repository. Add new H1 component (Header)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+
+
+import Header from './header/Header'
 
 class App extends Component {
 
   render() {
     return (
-      <div>
-        <p>Test if works!</p>
+      <div className="App">
+        <Header />
       </div>
     );
   }
 }
 
-export default App;
+export default App
